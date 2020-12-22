5153dd882ceb6915363f63ff61a9650e25b190a4
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2020-12-22 23:22:01 +0700

    Update day_3.py

--- code/day_3.py
+++ code/day_3.py
@@ -1,11 +1,16 @@
 # day 3 - part 1
 s=0
 import urllib.request
+from math import ceil
 url='https://raw.githubusercontent.com/truonghm/adventofcode/main/input/Day_3_Toboggan_Trajectory.txt'
-for num, line in enumerate(urllib.request.urlopen(url)):
-    row = line.decode('utf-8').replace('\n','').split(': ')[0]
-    if len(row) > num*3:
+tob = [line.decode('utf-8').replace('\n','') for line in urllib.request.urlopen(url)]
+
+for num, line in enumerate(tob):
+    row = line*len(tob)
+    try:
         if row[num*3]=='#':
-            print(num*3, row, row[num*3])
             s=s+1
+    except:
+        pass
+
 print(s)
