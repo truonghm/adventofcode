b80bebddc43fa4b5521155440b50f7570fd7ec24
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2020-12-24 00:19:00 +0700

    Update day_6.py

--- code/day_6.py
+++ code/day_6.py
@@ -2,7 +2,8 @@
 def open_input(path):
     with open(path,'r') as f:
         sections = f.read().split("\n\n")
-        answers=[section.split('\n') for section in sections]
+        # get rid of empty strings
+        answers=[[s for s in section.split('\n') if s] for section in sections]
 
     return answers
 
@@ -21,16 +22,12 @@ def day_6_part_1(answers):
 def day_6_part_2(answers):
     count=0
     for answer in answers:
-        unique_answer=get_unique(answer)
-        a_list=[]
-        for u in unique_answer:
-            if all(u in a for a in answer):
-                a_list.append(u)
+        joined_a = ''.join(answer)
+        unique_a=get_unique(answer)
+        a_list=[u for u in unique_a if joined_a.count(u)==len(answer)]
         count=count+len(a_list)
-                
-                
     return count
 
 answers=open_input(filepath+'Day_6_Custom_Customs.txt')
-# print(day_6_part_1(answers))
-print(day_6_part_2(answers))
+print('part 1:', day_6_part_1(answers))
+print('part 2:', day_6_part_2(answers))
