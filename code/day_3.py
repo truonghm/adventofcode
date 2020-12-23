64b096b0bc9b46aec26d0537fb2daea8edc2e63e
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2020-12-23 11:05:33 +0700

    Update day_3.py

--- code/day_3.py
+++ code/day_3.py
@@ -1,21 +1,23 @@
 # day 3
 
-import urllib.request
 from math import ceil
 
-def tree_count(right, down):
+def tree_count(path, right, down):
     s=0
-    url='https://raw.githubusercontent.com/truonghm/adventofcode/main/input/Day_3_Toboggan_Trajectory.txt'
-    tob = [line.decode('utf-8').replace('\n','') for line in urllib.request.urlopen(url)][::down]
+    with open(path, 'r') as f:
+        tob = [line.replace('\n','') for line in f][::down]
 
-    for num, line in enumerate(tob):
-        row = line*ceil((right*len(tob)/len(line)))
-        try:
-            if row[num*right]=='#':
-                s=s+1
-        except:
-            pass
+        for num, line in enumerate(tob):
+            row = line*ceil((right*len(tob)/len(line)))
+            try:
+                if row[num*right]=='#':
+                    s=s+1
+            except:
+                pass
 
     return s
 
-tree_count(1,1)*tree_count(3,1)*tree_count(5,1)*tree_count(7,1)*tree_count(1,2)
+path = filepath+'Day_3_Toboggan_Trajectory.txt'
+part_1 = tree_count(path,3,1)
+part_2 = tree_count(path,1,1)*tree_count(path,3,1)*tree_count(path,5,1)*tree_count(path,7,1)*tree_count(path,1,2)
+print(part_1, part_2)
