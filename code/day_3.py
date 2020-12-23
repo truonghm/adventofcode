934f6633c2ab75bf6e9d09207b1e54bf5219531c
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2020-12-23 09:54:12 +0700

    Update day_3.py

--- code/day_3.py
+++ code/day_3.py
@@ -1,16 +1,21 @@
-# day 3 - part 1
-s=0
+# day 3
+
 import urllib.request
 from math import ceil
-url='https://raw.githubusercontent.com/truonghm/adventofcode/main/input/Day_3_Toboggan_Trajectory.txt'
-tob = [line.decode('utf-8').replace('\n','') for line in urllib.request.urlopen(url)]
 
-for num, line in enumerate(tob):
-    row = line*len(tob)
-    try:
-        if row[num*3]=='#':
-            s=s+1
-    except:
-        pass
+def tree_count(right, down):
+    s=0
+    url='https://raw.githubusercontent.com/truonghm/adventofcode/main/input/Day_3_Toboggan_Trajectory.txt'
+    tob = [line.decode('utf-8').replace('\n','') for line in urllib.request.urlopen(url)][::down]
 
-print(s)
+    for num, line in enumerate(tob):
+        row = line*ceil((right*len(tob)/len(line)))
+        try:
+            if row[num*right]=='#':
+                s=s+1
+        except:
+            pass
+
+    return s
+
+tree_count(1,1)*tree_count(3,1)*tree_count(5,1)*tree_count(7,1)*tree_count(1,2)
