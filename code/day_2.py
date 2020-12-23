49399058329c9b098a23c10008fcf2d5ee33080a
Truong Hoang <10404416+truonghm@users.noreply.github.com>
2020-12-23 15:07:42 +0700

    Update day_2.py

--- code/day_2.py
+++ code/day_2.py
@@ -1,28 +1,32 @@
 # day 2
 
-def day_2_part_1(path):
+def open_input(path):
+    pw_list=[]
     with open(path,'r') as f:
-        s = 0
         for line in f:
             pw = line.replace('\n','').split(': ')
-            char = pw[0][-1]
-            char_count = pw[1].count(char)
-            lb = int(pw[0].split(' ')[0].split('-')[0])
-            ub = int(pw[0].split(' ')[0].split('-')[1])
-            if char_count >= lb and char_count <= ub:
-                s = s + 1
+            pw_list.append(pw)
+    return pw_list
+
+def day_2_part_1(path):
+    s = 0
+    for pw in open_input(path):
+        char = pw[0][-1]
+        char_count = pw[1].count(char)
+        lb = int(pw[0].split(' ')[0].split('-')[0])
+        ub = int(pw[0].split(' ')[0].split('-')[1])
+        if char_count >= lb and char_count <= ub:
+            s = s + 1
     return s
 
 def day_2_part_2(path):
-    with open(path,'r') as f:
-        s = 0
-        for line in f:
-            pw = line.replace('\n','').split(': ')
-            char = pw[0][-1]
-            id1 = int(pw[0].split(' ')[0].split('-')[0])
-            id2 = int(pw[0].split(' ')[0].split('-')[1])
-            if (pw[1][id1-1]==char or pw[1][id2-1]==char) and not (pw[1][id1-1]==char and pw[1][id2-1]==char):
-                s = s + 1
+    s = 0
+    for pw in open_input(path):
+        char = pw[0][-1]
+        id1 = int(pw[0].split(' ')[0].split('-')[0])
+        id2 = int(pw[0].split(' ')[0].split('-')[1])
+        if (pw[1][id1-1]==char or pw[1][id2-1]==char) and not (pw[1][id1-1]==char and pw[1][id2-1]==char):
+            s = s + 1
     return s
 
 filename = 'Day_2_Password_Philosophy.txt'
